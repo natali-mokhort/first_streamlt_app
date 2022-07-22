00867566f5a6a56492bc7b227fd5efdf3e2c1850
natali-mokhort <103184315+natali-mokhort@users.noreply.github.com>
2022-07-22 22:36:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page 
 streamlit.dataframe(fruits_to_show)
-
+streamlit.header('Hello Natali')
 # Create the repeatable code block (called a fuction)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
